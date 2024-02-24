d2eb0f0a839214097c94613a5cce756cbc25081d
modestminer <85529383+inkedinlove@users.noreply.github.com>
2024-02-24 17:35:37 -0500

    Stellar Cdnjs file added to the Script file

---
+++ Scripts/StellarCdnjs.js
@@ -0,0 +1,19 @@
+﻿//Optional Alternative to using the StellarSdk package: Current Version as of 2024-24-02
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/stellar-sdk/11.2.2/stellar-sdk.js"></script>
+
+
+//Example of how to use the StellarSdk package via CDNJS
+/*----------------------------------------------------------*/
+
+/*var StellarSdk = require('stellar-sdk');
+var server = new StellarSdk.Server('https://horizon-testnet.stellar.org');
+
+server.loadAccount('GASOCNHNNLYFNMDJYQ3XFMI7BYHIOCFW3GJEOWRPEGK2TDPGTG2E5EDW')
+    .then(function (account) {
+        console.log('Balances for account: ' + accountId);
+        account.balances.forEach(function (balance) {
+            console.log('Type:', balance.asset_type, ', Balance:', balance.balance);
+        });
+    });*/
